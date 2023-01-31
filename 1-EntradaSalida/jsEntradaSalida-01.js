b180e39134cd674cfcbe118320a2d6d6a9705ed2
daiidlmuela <delamuela90@gmail.com>
2023-01-31 13:11:18 -0300

    volvi a modificar la alerta

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
 /*
+daiana de la muela
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla");
 }
 
