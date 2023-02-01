e1580c4a03d8077fb9cb74c33bd7796382e9f37d
daiidlmuela <delamuela90@gmail.com>
2023-02-01 10:19:14 -0300

    ejercicio 3,4 y 5

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,18 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert( "Su nombre es "+nombreIngresado);
+	let nombreIngresado;
+let mensaje;
+	//nombreIngresado=txtIdNombre.value; ESTO FUNCIONA SOLO EN GOOGLE
+
+      nombreIngresado =document.getElementById("txtIdNombre").value;
+
+mensaje = "Vos te llamas " +nombreIngresado
+
+	alert(mensaje);
+
+
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,18 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+
+	//id nombre txtIdNombre
+	//id edad txtIdEdad
+
+	let nombreIngresado;
+	let edadIngresada;
+	let mensaje;
+
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+	edadIngresada =document.getElementById("txtIdEdad").value;
+	mensaje="Usted se llama "+ nombreIngresado + " y tiene " +edadIngresada +" años";
+
+	alert(mensaje);
 }
 
