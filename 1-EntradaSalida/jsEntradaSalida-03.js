14b98ee2b359943f0584c5f377223888dc089cb5
daiidlmuela <delamuela90@gmail.com>
2023-02-01 22:41:12 -0300

    e/s 3 y bis3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,13 +3,32 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombreIngresado;
-let mensaje;
+	//let nombreIngresado;
+	//let mensaje;
 	//nombreIngresado=txtIdNombre.value; ESTO FUNCIONA SOLO EN GOOGLE
 
-      nombreIngresado =document.getElementById("txtIdNombre").value;
+    //  nombreIngresado =document.getElementById("txtIdNombre").value;
 
-mensaje = "Vos te llamas " +nombreIngresado
+	//mensaje = "Vos te llamas " +nombreIngresado
+
+	//alert(mensaje);
+
+	//ejercicio 3bis
+
+	let descripcion;
+	let precio;
+	let mensaje;
+	let porcentaje;
+	
+
+	descripcion = prompt("Crema de manos");
+	precio = prompt(document.getElementById("txtIdNombre").value);
+
+	precio = 500;
+	porcetaje = precio * 30/100;
+
+
+	mensaje= (descripcion + " su precio es " + porcentaje);
 
 	alert(mensaje);
 
