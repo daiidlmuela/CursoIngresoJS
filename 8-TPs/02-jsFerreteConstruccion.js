d98995f3ff87a8d05ebef09808cb76e7f89aaab6
daiidlmuela <delamuela90@gmail.com>
2023-02-02 19:41:18 -0300

    tp2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,9 +6,7 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-    txtIdLargo
-    txtIdAncho
-    txtIdRadio
+    
     let largo;
     let ancho;
     let perimetro;
@@ -37,6 +35,34 @@ function Circulo ()
 {
 	// mostrar la cantidad de alambre a comprar si se ingresara el radio de un terreno circular y se debe a alambre con ilos de
     //alambre.sacar el perimetro de un circulo hay que hacer 2 * pi * r*/
+    //txtIdLargo
+    //txtIdAncho
+    //txtIdRadio
+    
+    let radio;
+    let perimetro;
+    let alambre;
+    let PI;
+    let cantAlambre;
+    let cantidadTotal;
+
+    PI = 3.14;
+
+    radio = document.getElementById ("txtIdRadio").value;
+    radio = parseFloat (radio);
+
+    cantAlambre= 2 * PI * radio;
+    cantidadTotal= cantAlambre * 3;
+
+    mensaje = ("La cantidad de alambre que se necesita es: " + cantidadTotal );
+
+     console.log(mensaje);
+
+
+
+
+
+
 
 
 
@@ -48,6 +74,32 @@ function Materiales ()
     //C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 	//para sacar el area de un rectangulo se hace ancho * largo
     //para sacar las bolsas de cemento multiplico mentro cuadrado por cemento y luego cal */
+
+    let area;
+    let cemento;
+    let cal;
+    
+
+    cemento = 2;
+    cal = 3;
+
+    largo = document.getElementById ("txtIdLargo").value;
+    ancho = document.getElementById ("txtIdAncho").value;
+
+    
+    largo = parseFloat(largo);
+    ancho = parseFloat(ancho);
+
+    area = ancho * largo;
+    
+    cemento = area * 2 / 1;
+
+    console.log("Se necesitan " + cemento + " bolsas de cemento")
+
+
+
+
+
 
 
 }
