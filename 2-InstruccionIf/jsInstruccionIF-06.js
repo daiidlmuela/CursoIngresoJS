a314135f780e2ed48e29276bf5155d6690128c9b
daiidlmuela <delamuela90@gmail.com>
2023-02-07 12:06:37 -0300

    if ejercicio 6

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,37 @@
+
+/*Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años)
+ o adolescente (entre 13 y 17 años) o niño (menor a 13 años).*/
+
+
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//txtIdEdad
+	let edad;
+	let mensaje;
+
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad);
+
+	if(edad > 17 )
+	{
+		mensaje = "Mayor de edad";
+		
+
+	} else {
+		
+	 if (edad >= 13 )
+	 {
+		mensaje = "Es adolescente ";	
+
+	}else{
+
+		mensaje = " Es menor de edad";
+	}
 
 
 
+}
+	alert (mensaje);
+	
 }//FIN DE LA FUNCIÓN
