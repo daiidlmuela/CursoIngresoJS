868da861dc9b4bf6c599d0532b6b5078206929d0
daiidlmuela <delamuela90@gmail.com>
2023-02-08 10:37:02 -0300

    ejercicio 10 if

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,37 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	/*Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4*/
+	
+let nota;
+let mensaje;
+
+
+nota =Match.floor( Math.random() *10 + 1 );
+
+if(nota >= 9)
+{
+
+mensaje= nota + " Exelente";
+
+}
+else{
+	if(nota >= 4)
+	{
+		mensaje = nota + " Aprobado";
+
+	}
+	else{
+
+		mensaje =  nota + " Reprobado";
+	}
+	
+
+
+}
+	alert (mensaje);
+
 
 }//FIN DE LA FUNCIÓN
