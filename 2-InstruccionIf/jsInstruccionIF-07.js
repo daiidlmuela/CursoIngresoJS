0e7320714e2acfc223252f11e0d6348c174c634f
daiidlmuela <delamuela90@gmail.com>
2023-02-08 09:32:37 -0300

    ejercicio 7

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,24 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	
+	
+	 let edad;
+	 let civil;
+	let mensaje;
+
+	 edad = document.getElementById("txtIdEdad").value;
+	 civil = document.getElementById("estadoCivil").value;
+
+	 edad = parseInt(edad);
+
+	 if ( edad <18 && civil != "Soltero")	
+	{
+		mensaje = "Es muy pequeño para no ser soltero"
+		alert(mensaje);
+
+	}
+
 	
 
 
-}//FIN DE LA FUNCIÓN
+	}//FIN DE LA FUNCIÓN
