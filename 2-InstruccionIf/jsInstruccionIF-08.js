5a1a30e0903cfa7088bfaaa6853f2a472b836cb1
daiidlmuela <delamuela90@gmail.com>
2023-02-08 09:41:55 -0300

    ejercicio 8 if

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,24 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	/*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'*/
+
+let edad;
+let civil;
+let mensaje;
+
+
+edad = document.getElementById("txtIdEdad").value;
+edad = parseInt(edad);
+
+civil = document.getElementById("estadoCivil").value;
+
+if(edad >= 18 && civil == "Soltero" )
+{
+mensaje = "Es soltero y no es menor"
+	alert(mensaje);
+
+}
 
 
 }//FIN DE LA FUNCIÓN
