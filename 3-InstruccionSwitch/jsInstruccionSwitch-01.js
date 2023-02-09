35ddb2f4a90a2f1b87d463a5668c80ebbee39efe
daiidlmuela <delamuela90@gmail.com>
2023-02-09 12:10:22 -0300

    switch 1

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,34 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	/*al seleccionar un mes informar.
+si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiesta!!!."*/
 
+	let mesDelAño;
+	let mensaje;
+	
+	mesDelAño = document.getElementById("txtIdMes").value;
 
+	switch(mesDelAño)
+	{
+		case "Enero":
+			mensaje = "que comiences bien el año!!!."
+			break;
+		case "Marzo":
+			mensaje = "a clases!!!."
+			break;
+		case "Julio":
+			mensaje = "se vienen las vacaciones!!!."
+			break;
+		case "Diciembre":
+			mensaje = "Felices fiesta!!!."
+			break;
+		default: 
+			mensaje = "Es un mes aburrido"
+	}
+
+			alert (mensaje)
 
 }//FIN DE LA FUNCIÓN
