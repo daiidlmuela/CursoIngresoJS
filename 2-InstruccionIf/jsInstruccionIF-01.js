203d8ec8863a84c965af6a019564f923e9b9a134
daiidlmuela <delamuela90@gmail.com>
2023-02-09 08:57:21 -0300

    if 1

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -2,7 +2,7 @@ function mostrar()
 {
 	//txtIdEdad
 
-	let edad;
+	/*let edad;
 
 	edad = document.getElementById("txtIdEdad").value; 
 	edad = parseInt(edad);
@@ -10,6 +10,40 @@ function mostrar()
 	if(edad == 15) 
 	{
 		alert("Niña bonita")
-	}
+	}*/
 
-}//FIN DE LA FUNCIÓN
+}
+function mostrar()
+{
+	/*Ejercicio 1bis: 
+/con if
+pedir a una persona que fue de vaciones , 
+la distancia que viajo y el tiempo que tardo, para calcular la velocidad
+e informar:
+60 km/hr = muy lento 
+hata 80km/h= lento 
+hasta 100= buen ritmo
+hasta 120= ahi de la ley 
+mas = eso no se hace/ */
+
+let velocidad;
+let mensaje;
+
+velocidad = document.getElementById("txtIdEdad").value;
+velocidad = parseInt (velocidad);
+
+if (velocidad == 60)
+{
+	mensaje = velocidad + " muy lento";
+	alert (mensaje)
+
+}else if (velocidad == 80 )
+{
+	mensaje = velocidad + " lento";
+	alert (mensaje)
+}else if(velocidad == 100)
+	{
+		mensaje = velocidad + " Buen ritmo";
+		alert(mensaje)
+	}
+}
