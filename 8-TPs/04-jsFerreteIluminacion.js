b6766eb9952a23ec9adaa2c78a7990c1438ee425
daiidlmuela <delamuela90@gmail.com>
2023-02-09 08:55:12 -0300

    tp 4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -7,8 +7,85 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
+txtIdCantidad
+txtIdprecioDescuento
  */
 function CalcularPrecio () 
 {
  	
+
+let cantidad;
+let descuento;
+let mensaje;
+let precioUnitario;
+let marca;
+let precioTotal;
+
+
+precioUnitario = 35;
+descuento = 50;
+
+cantidad = document.getElementById("txtIdCantidad").value;
+cantidad = parseInt(cantidad);
+marca = document.getElementById("Marca").value;
+
+
+
+if(cantidad >=6)
+{
+   precioTotal = precioUnitario * cantidad ;
+   precioTotal = precioTotal * descuento/100;
+   // precioFinal = total - descuento; 
+
+   // mensaje = total;
+    document.getElementById("txtIdprecioDescuento").value = precioTotal;
+
+} else if(cantidad ==5 && marca == "ArgentinaLuz")
+  {  descuento = 1- 40/100;
+    precioTotal= precioUnitario * cantidad;
+    precioTotal = precioTotal * descuento;
+
+    document.getElementById("txtIdprecioDescuento").value = precioTotal;
+    
+} 
+    else if(cantidad ==5 && marca != "ArgentinaLuz")
+ {descuento = 1 - 30/100;
+ precioTotal= precioUnitario * cantidad;
+precioTotal = precioTotal * descuento;
+
+document.getElementById("txtIdprecioDescuento").value = precioTotal;
+} else if (cantidad == 4 && marca == "ArgentinaLuz" || "FelipeLamparas")
+{
+    descuento = 1 - 25/100;
+    precioTotal= precioUnitario * cantidad;
+   precioTotal = precioTotal * descuento;
+   
+   document.getElementById("txtIdprecioDescuento").value = precioTotal;
+
+}else if (cantidad == 4 && (marca != "ArgentinaLuz" || marca != "FelipeLamparas"))
+{ 
+    descuento =  20/100;
+    precioTotal= precioUnitario * cantidad;
+   precioTotal = precioTotal * descuento; //me da mal el resultado
+   
+   document.getElementById("txtIdprecioDescuento").value = precioTotal;
+
+}else if(cantidad == 3 && marca == "ArgentinaLuz")
+{
+    descuento = 1 -  15/100;
+    precioTotal= precioUnitario * cantidad;
+   precioTotal = precioTotal * descuento;//me da mal el resultado
+   
+   document.getElementById("txtIdprecioDescuento").value = precioTotal;
+
+
 }
+}
+
+
+
+
+
+
+
+
