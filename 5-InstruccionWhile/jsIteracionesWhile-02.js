923e7a14486717a7ffe40be1c34254961ad40b0f
daiidlmuela <delamuela90@gmail.com>
2023-02-16 13:35:52 -0300

    while

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,7 +1,7 @@
 /*
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
-function mostrar()
+/* function mostrar()
 {
 	let i;
 
@@ -14,4 +14,38 @@ function mostrar()
 		console.log (i)
 	}
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN */
+function mostrar()
+{
+/* 	while 2 bis: Ingresar 5 números, y determinar la cantidad de números que son mayores que 10 
+y menores 20 (inclusive en ambos casos)*/
+
+	let numeroIngresado;
+	let contador;
+	let contadorMayor;
+	let contadorMenor;
+	let respuesta;
+
+	respuesta = "si"
+	contador = 5;
+
+	while (respuesta == "si")
+	{
+		numeroIngresado= parseInt(prompt("Ingrese un numero"))
+
+		while(isNaN(numeroIngresado)) 
+		{
+			numeroIngresado = parseInt(prompt("ERROR:Ingrese un numero "))
+		}
+		if(numeroIngresado >10 )
+		{
+			contadorMayor = contadorMayor + 1;
+		}
+		else if (numeroIngresado < 20)
+		{
+			contadorMenor = contadorMenor + 1;
+		}
+		numeroIngresado= parseInt(prompt("desea ingresar otro un numero: si / no"))
+
+	}
+}
